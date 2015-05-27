f1ce8099db412a39040db0da2009b8f18703d7fd
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2015-05-27 09:27:50 +0200

    0.13

--- lib/RDF/LDF.pm
+++ lib/RDF/LDF.pm
@@ -21,7 +21,7 @@ use JSON;
 use URI::Template;
 use RDF::LDF::Error;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 {
    my $ua   = RDF::Trine->default_useragent;
    $ua->agent("RDF:::LDF/$RDF::LDF::VERSION " . $ua->_agent);
