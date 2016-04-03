38ec50fd4942f3ea5655db0b18efcddbb0089c98
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2016-04-03 17:38:51 +0200

    Better accept header passing to RDF::Trine

--- lib/RDF/LDF.pm
+++ lib/RDF/LDF.pm
@@ -474,10 +474,12 @@ sub get_fragment {
     
     # JSON support in RDF::Trine isn't JSON-LD
     # Set the accept header quality parameter at a minimum for this format
-    RDF::Trine->default_useragent->default_header('Accept','text/turtle;q=1.0,application/turtle;q=1.0,application/x-turtle;q=1.0,application/rdf+xml;q=0.9,text/x-nquads;q=0.9,application/json;q=0.1,application/x-rdf+json;q=0.1');
+    my $ua = RDF::Trine->default_useragent;
+    $ua->default_header('Accept','text/turtle;q=1.0,application/turtle;q=1.0,application/x-turtle;q=1.0,application/rdf+xml;q=0.9,text/x-nquads;q=0.9,application/json;q=0.1,application/x-rdf+json;q=0.1');
 
     eval {
-        RDF::Trine::Parser->parse_url_into_model($url, $model);
+        # Need to explicitly set the useragent to keep the accept headers
+        RDF::Trine::Parser->parse_url_into_model($url, $model, useragent => $ua);
     };
     
     if ($@) {
